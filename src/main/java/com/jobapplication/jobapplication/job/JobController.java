125120848326353159df2770fce88aef05bea8c9
shiundu zachariah <brianzion63@gmail.com>
2024-03-20 20:18:43 +0300

    Update

--- src/main/java/com/jobapplication/jobapplication/job/JobController.java
+++ src/main/java/com/jobapplication/jobapplication/job/JobController.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class JobController {
     private List<job> jobs = new ArrayList<>();
 
